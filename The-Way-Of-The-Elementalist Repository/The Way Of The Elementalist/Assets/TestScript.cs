6cc2e4167c12f4c73c22260d82f171621e92e04a
Connor <Connormckell@hotmail.com>
2021-02-22 12:53:27 +0000

    Added a platform test asset. added a testscript for the project.

---
+++ The-Way-Of-The-Elementalist Repository/The Way Of The Elementalist/Assets/TestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    //
+    public string startText;
+    public string updateText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(startText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(updateText);
+    }
+}
