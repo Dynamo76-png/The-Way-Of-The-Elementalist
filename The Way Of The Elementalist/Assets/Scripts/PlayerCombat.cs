009788c77ec69d1b82351009961fb962db26adbe
Connor <Connormckell@hotmail.com>
2021-04-23 13:33:51 +0100

    Prefabs made, Healing Point made.
    Prefabs have been made for Leilani, a spike and a Healing Orb. Players can now heal themselves by using a Healing Orb.

--- The Way Of The Elementalist/Assets/Scripts/PlayerCombat.cs
+++ The Way Of The Elementalist/Assets/Scripts/PlayerCombat.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //Play Attack Animation
+
+        //Detect Enemy in Attack Radius
+        //Damage Enemy
+
     }
 }
