61cef3161558325f75e384a32b115b12f8c7498f
Connor <Connormckell@hotmail.com>
2021-05-24 11:08:58 +0100

    Simple Combat is avaliable,
    Players can now move quicker and fall faster, and has the option to attack Fire Mechs, destroying them in one attack.

---
+++ The Way Of The Elementalist/Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+
+    // CONDITION: When the projectile hits a certain object type... (enemy)
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Check if the object we collided with has the tag we are looking for (Enemy)
+        if (otherCollider.CompareTag("Enemy") == true)
+        {
+            // Perform our action
+            KillEnemy(otherCollider.gameObject);
+        }
+    }
+
+
+    // ACTION: Destroy an object (enemy)
+    public void KillEnemy(GameObject enemy)
+    {
+        Destroy(enemy);
+    }
+
+}
